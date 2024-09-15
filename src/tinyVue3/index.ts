b1150b872e0d096abccd853001e107eb20c3dc24
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-09-15 15:26:05 +0800

    chore: init

---
+++ src/tinyVue3/index.ts
@@ -0,0 +1,21 @@
+import directives from '../directives.json'
+import { getComponentMap, getPropsMap } from './mapping'
+
+export function tinyVue3() {
+  return {
+    uiName: '@opentiny/vue3',
+    map: getPropsMap(),
+    lib: '@opentiny/vue',
+    prefix: 'tiny',
+  }
+}
+
+export function tinyVue3Components() {
+  return {
+    map: getComponentMap(),
+    isSeperatorByHyphen: true,
+    prefix: 'tiny',
+    lib: '@opentiny/vue',
+    directives: directives.tinyVue3,
+  }
+}
